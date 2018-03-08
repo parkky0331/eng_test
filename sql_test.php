22565342b73a25d541ecd1c0cbfe7b7d9298bb7d
parkky0331 <parkky0331>
2018-03-09 00:14:48 +0900

    랜덤-2
    별개의 다른 뜻 각 단어당 3개씩 같이 출력

--- sql_test.php
+++ sql_test.php
@@ -10,11 +10,14 @@ $connect = mysqli_connect("localhost","parkky0331","parkky0331_db", "parkky0331"
 	<title></title>
 </head>
 <body>
-	<table width="640" border="1" cellpadding="5">
+	<table width="1024" border="1" cellpadding="5">
 		<tr align="center" bgcolor="#00FF62">
 			<td>num</td>
 			<td>eng</td>
 			<td>mean</td>
+			<td>answer</td>
+			<td>answer</td>
+			<td>answer</td>
 		</tr>
 		<?
 		for ($count=0; $count < 10 ; $count++) { 
@@ -27,16 +30,31 @@ $connect = mysqli_connect("localhost","parkky0331","parkky0331_db", "parkky0331"
 			$result = mysqli_query($connect, $sql);
 			$row = mysqli_fetch_array($result);
 
-			echo "<tr align='center'>
+			echo "
+			<tr align='center'>
 			<td>$row[word_num]</td>
 			<td>$row[word_eng]</td>
-			<td>$row[word_mean]</td>
-			</tr>";
+			<td>$row[word_mean]</td>";
+
+
+			for ($count_a=0; $count_a <3 ; $count_a++) { 
+			# code...
+				$value_a = mt_rand(1,100);
+				$dbArray_a[$count_a] = $value_a;
+
+				$sql = "select word_mean from test_list where word_num = '$dbArray_a[$count_a]'";
+
+				$result_a = mysqli_query($connect, $sql);
+				$row_a = mysqli_fetch_array($result_a);
+				echo "
+				<td>$row_a[word_mean]</td>";
 			}
-			mysqli_close($connect);
+			echo "</tr>";
+		}
+		mysqli_close($connect);
 		?>
 
-</table>
+	</table>
 
 </body>
 </html>
